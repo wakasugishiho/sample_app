deac7ffabdb12121c41fc5cd3fdb0ca4a6543e35
若杉志帆 <wakasugishiho@wakasugishihonoMacBook-Air.local>
2019-10-08 17:48:40 +0900

    [finish]3-6

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
 	get 'top' => 'root#top'
 	post 'todolists' => 'todolists#create'
 	get 'todolists' => 'todolists#index'
+	get 'todolists/:id' => 'todolists#show',as:'todolist'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
